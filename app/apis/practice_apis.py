cb33f643bfafc81aaf00fa570e0306eb2b133c04
donghun <sdh080200@gmail.com>
2026-07-14 17:31:28 +0900

    feat: 회원 생성 API 추가

--- app/apis/practice_apis.py
+++ app/apis/practice_apis.py
@@ -1,4 +1,7 @@
-# app/apis/practice_apis.py
+import re
+from fastapi import FastAPI
+from pydantic import BaseModel, field_validator, Field
+
 user_list = [
 	{
 		"id": 1,
@@ -22,3 +25,46 @@ user_list = [
 		"password": "lwsPAssword12@"
 	}
 ]
+
+class UserSignUpRequest(BaseModel):
+    name: str = Field(..., min_length=2, max_length=10)
+    age: int = Field(..., ge=14)
+    email: str = Field(..., max_length=30)
+    password: str = Field(..., min_length=8, max_length=20)
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, password: str):
+        if not re.search(r'[A-Z]', password):
+            raise ValueError('password must contain at least one uppercase letter')
+        if not re.search(r'[a-z]', password):
+            raise ValueError('password must contain at least one lowercase letter')
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
+            raise ValueError('password must contain at least one special character')
+        return password
+
+    @field_validator("email")
+    @classmethod
+    def validate_email(cls, email: str):
+        if not re.search(r'@', email):
+            raise ValueError('email must contain @')
+        return email
+
+
+@app.post(
+    "/users",
+    summary = "회원 생성 API",
+)
+def create_user(user: UserSignUpRequest):
+    new_id = max(
+        (saved_user["id"] for saved_user in user_list), default=0
+    ) + 1
+    new_user = {
+        "id": new_id,
+        "name": user.name,
+        "age": user.age,
+        "email": user.email,
+        "password": user.password,
+    }
+    user_list.append(new_user)
+    return new_user
