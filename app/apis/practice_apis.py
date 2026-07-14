f8a6dc15ffe8ad036d33e739ca98714b4307c17c
Lil Spicy <78456018+dddong1234@users.noreply.github.com>
2026-07-14 18:16:11 +0900

    feat: 회원 생성 API 추가 (#10)
    * feat: 회원 생성 API 추가
    
    * refactor: 회원 생성 API를 APIRouter 구조로 변경

--- app/apis/practice_apis.py
+++ app/apis/practice_apis.py
@@ -35,6 +35,54 @@ user_list = [
 	}
 ]
 
+
+#==========================[회원생성 api]===============================#
+class UserSignUpRequest(BaseModel):
+    name: str = Field(..., min_length=2, max_length=10)
+    age: int = Field(..., ge=14)
+    email: str = Field(..., max_length=30)
+    password: str = Field(..., min_length=8, max_length=20)
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, password: str):
+        if not re.search(r'[A-Z]', password):
+            raise ValueError('password must contain at least one uppercase letter')
+        if not re.search(r'[a-z]', password):
+            raise ValueError('password must contain at least one lowercase letter')
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
+            raise ValueError('password must contain at least one special character')
+        return password
+
+    @field_validator("email")
+    @classmethod
+    def validate_email(cls, email: str):
+        if not re.search(r'@', email):
+            raise ValueError('email must contain @')
+        return email
+
+
+@router.post(
+    "/users",
+    summary="회원 생성 API",
+)
+def create_user(user: UserSignUpRequest):
+    new_id = max(
+        (saved_user["id"] for saved_user in user_list), default=0
+    ) + 1
+    new_user = {
+        "id": new_id,
+        "name": user.name,
+        "age": user.age,
+        "email": user.email,
+        "password": user.password,
+    }
+    user_list.append(new_user)
+    return new_user
+  
+  
+  
+#==========================[회원정보 수정 api]===============================#
 class UserUpdate(BaseModel):
     age: int | None = Field(
         default=None,
